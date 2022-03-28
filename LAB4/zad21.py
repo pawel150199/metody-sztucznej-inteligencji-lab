c6e79711836c8ab9e61c999ad3809c9ef029d892
pawel150199 <pawel.polski99@gmail.com>
2022-03-28 17:24:25 +0200

    Drobne poprawki

---
+++ LAB4/zad21.py
@@ -0,0 +1,75 @@
+from tabnanny import check
+from sklearn.base import BaseEstimator, ClassifierMixin
+import numpy as np
+import random
+from sklearn.datasets import make_classification
+from sklearn.neighbors import DistanceMetric
+from sklearn.utils import check_random_state
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.ensemble import RandomForestClassifier
+from numpy.random import MT19937
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from numpy.random import RandomState
+
+
+class RandomClassifier(BaseEstimator, ClassifierMixin):
+    """An example of classifier"""
+
+    def __init__(self):
+        """Inicjalizacja generatora"""
+
+    def fit(self, X, y):
+        """Uczenie się"""
+        # Check that X and y have correct shape
+        X, y = check_X_y(X, y)
+        # przechowujemy unikalne klasy problemu
+        self.classes_ = np.unique(y)
+        # przechowujemy X i y
+        self.X_, self.y_ = X, y
+        # liczba klas problemu
+        self.n_features = len(np.unique(y))
+        self.p = []
+        for i in self.classes_:
+            tmp = 0
+            for j in y:
+                if i == j:
+                    tmp += 1
+            self.p.append(tmp)
+        ix = self.p.index(max(self.p)) 
+        self.max = self.y_[ix]    
+        return self
+
+    def predict(self, X):
+        """Dokonujemy predykcji"""
+        check_is_fitted(self)  # sprawdzam czy jest wywołana metoda fit
+        X = check_array(X)
+
+        y_predict = [self.max for i in range(0, len(X))]
+        return y_predict
+
+
+if __name__ == "__main__":
+    X, y = make_classification(
+        n_samples=200,  # liczba generowanych wzorców
+        n_features=2,  # liczba atrybutów
+        n_classes=2,  # liczba klas problemu 2->problem binarny
+        n_repeated=0,  # brak powtórzeń
+        n_redundant=0,  # brak cech zbędnych
+        # flip_y = 0.08,#szum etykiet 8% ogółu wzorca
+        # random_state=1500,
+        # weights=None
+        n_informative=2,
+    )
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=2
+    )
+
+    clf = RandomClassifier()
+    clf.fit(X_train, y_train)
+    predict = clf.predict(X_test)
+    score = accuracy_score(y_test, predict)
+
+    print(f"Accuracy Score: {score.round(2)}")
---
+++ LAB4/zad23.py
@@ -0,0 +1,69 @@
+from zad21 import RandomClassifier
+from zad22 import NClassifier
+import numpy as np
+from sklearn import datasets
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import RepeatedStratifiedKFold
+from prettytable import PrettyTable as table
+
+
+clfs = {
+    "RC": RandomClassifier(),
+    "kNN": NClassifier(),
+}
+
+data = [
+    datasets.make_moons(),
+    datasets.make_circles(),
+    datasets.make_blobs(random_state=42),
+]
+n_data = len(data)
+n_splits = 2
+n_repeats = 5
+rskf = RepeatedStratifiedKFold(
+    n_splits=n_splits, n_repeats=n_repeats, random_state=1234
+)
+scores = np.zeros((len(clfs), n_data, n_splits * n_repeats))
+
+for i in range(0, len(data)):
+    d = data[i]
+    X = d[0]
+    y = d[1]
+    for fold_id, (train, test) in enumerate(rskf.split(X, y)):
+        for clf_id, clf_name in enumerate(clfs):
+            clf = clfs[clf_name]
+            clf.fit(X[train], y[train])
+            y_pred = clf.predict(X[test])
+            scores[clf_id, i, fold_id] = accuracy_score(y[test], y_pred)
+
+# zapisuje wartości srednie i odchylenie standardowe
+mean_scores = np.mean(scores, axis=2).T
+std_scores = np.std(scores, axis=2).T
+
+
+# zapisanie wyników do tabel
+data_names = ["moons", "circles", "blob"]
+rc = []
+knn = []
+for i in range(0, n_data):
+    rc.append(round(mean_scores[i, 0], 3))
+    knn.append(round(mean_scores[i, 1], 3))
+
+# zapisuwanie wartosci odchylenia  standardowego
+s_scores = table()
+s_scores.field_names = ("nazwa zbioru", "Random Classifier",  "kNN Clasifier")
+rc_s = []
+knn_s = []
+for i in range(0, n_data):
+    rc_s.append(round(std_scores[i, 0], 3))
+    knn_s.append(round(std_scores[i, 1], 3))
+
+for i in range(0, len(data_names)):
+    s_scores.add_row([data_names[i], str(f"{rc[i]}\n({rc_s[i]})\n\n"), str(f"{knn[i]}\n({knn_s[i]})\n\n")])
+
+
+print("\n\nWyniki doświadczenia: \n\n")
+print(s_scores)
