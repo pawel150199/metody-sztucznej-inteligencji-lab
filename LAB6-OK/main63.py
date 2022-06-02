e1b458045f540decd19483b47958ad53a9d822d1
pawel150199 <pawel.polski99@gmail.com>
2022-06-02 14:47:39 +0200

    Corrections

--- LAB6-OK/main63.py
+++ LAB6-OK/main63.py
@@ -7,6 +7,7 @@ from zad63 import RandomSubspaceEnsemble
 from zad62 import BaggingClassifier2
 from sklearn.tree import DecisionTreeClassifier
 from tabulate import tabulate
+from scipy.stats import ttest_ind
 
 datasets = ['banana', 'balance', 'appendicitis', 'iris']
 
@@ -21,7 +22,7 @@ clfs = {
 
 n_repeat = 5
 n_split = 2
-scores = np.zeros((len(clfs), len(datasets), n_split*n_repeat))
+scores = np.zeros((len(datasets), len(clfs), n_split*n_repeat))
 rskf = RepeatedStratifiedKFold(n_splits=n_split, n_repeats=n_repeat, random_state=1410)
 
 for data_id, dataset in enumerate(datasets):
@@ -33,23 +34,44 @@ for data_id, dataset in enumerate(datasets):
             clf = clfs[clf_name]
             clf.fit(X[train], y[train])
             y_pred = clf.predict(X[test])
-            scores[clf_id, data_id, fold_id] = accuracy_score(y[test], y_pred)
+            scores[data_id, clf_id, fold_id] = accuracy_score(y[test], y_pred)
 
-mean = np.mean(scores, axis=2)
-std = np.std(scores, axis=2)
-#for clf_id, clf_name in enumerate(clfs):
-    #print("%s: %.3f (%.3f)" % (clf_name, mean[clf_id], std[clf_id]))
 
-headers = list(clfs.keys())
-headers.insert(0, "dataset\clf")
-n_column = np.reshape(np.array(datasets), (len(datasets),1))
-mean = np.mean(scores, axis=2).T
-std = np.std(scores, axis=2).T
+alpha=.05
+m_fmt="%.3f"
+std_fmt=None
+nc="---"
+db_fmt="%s"
+tablefmt="plain"
+clfs = list(clfs.keys())
+n_clfs = len(clfs)
+mean_scores = np.mean(scores, axis=2)
+stds = np.std(scores, axis=2)
 
-table = np.concatenate((n_column, mean), axis=1)
-table = tabulate(table, headers, floatfmt=".2f")
-print("Wartości Średnie:\n\n\n", table)
 
-table_2 = np.concatenate((n_column, std), axis=1)
-table_2 = tabulate(table_2, headers, floatfmt=".2f")
-print("\n\nOdchylenie standardowe:\n\n\n", table_2)
+t = []
+for db_idx, db_name in enumerate(datasets):
+    # Wiersz z wartoscia srednia
+    t.append([db_fmt % db_name] + [m_fmt % v for v in mean_scores[db_idx, :]])
+    # Jesli podamy std_fmt w zmiennych globalnych zostanie do tabeli dodany wiersz z odchyleniem standardowym
+    if std_fmt:
+        t.append([''] + [std_fmt % v for v in stds[db_idx, :]])
+    # Obliczenie wartosci T i P
+    T, p = np.array(
+        [[ttest_ind(scores[db_idx, i, :],
+            scores[db_idx, j, :])
+        for i in range(len(clfs))]
+        for j in range(len(clfs))]
+    ).swapaxes(0, 2)
+    _ = np.where((p < alpha) * (T > 0))
+    conclusions = [list(1 + _[1][_[0] == i])
+                for i in range(n_clfs)]
+    t.append([''] + [", ".join(["%i" % i for i in c])
+                                     if len(c) > 0 and len(c) < len(clfs)-1 else ("all" if len(c) == len(clfs)-1 else nc)
+                                     for c in conclusions])
+    
+headers = ['metrics', 'datasets']
+for i in clfs:
+    headers.append(i)
+
+print('\n\n',tabulate(t, headers))
